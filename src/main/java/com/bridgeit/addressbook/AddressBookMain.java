afc159c0b1c4c7a152ca0ff38a0736d962cc359c
Naincy224 <pihusingh224@gmail.com>
2021-11-13 00:18:37 +0530

    Added: welcome message program to address book

---
+++ src/main/java/com/bridgeit/addressbook/AddressBookMain.java
@@ -0,0 +1,16 @@
+package com.bridgeit.addressbook;
+
+public class AddressBookMain {
+	
+	public static void main(String[] args) {
+		printWelcomeMessage();
+		
+	}
+	
+	public static void printWelcomeMessage() {
+
+		System.out.println("Welcome to AddressBook system Program");
+
+	}
+
+}
